4734d7ff17fcf4ad5934560dab8e533bc17ac30c
miro <jarbasai@mailfence.com>
2026-01-09 00:13:10 +0000

    refactor: modernize/drop speech_recognition

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ CONFIG_ENTRY_POINT = (
 setup(
     name='ovos-stt-plugin-vosk',
     version=get_version(),
-    description='A vosk stt plugin for mycroft',
+    description='A vosk stt plugin for OpenVoiceOS',
     url='https://github.com/OpenVoiceOS/ovos-stt-plugin-vosk',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
@@ -73,21 +73,9 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Text Processing :: Linguistic',
         'License :: OSI Approved :: Apache Software License',
-
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
     ],
     keywords='mycroft ovos plugin stt',
-    entry_points={'mycroft.plugin.stt': PLUGIN_ENTRY_POINT,
-                  'mycroft.plugin.stt.config': CONFIG_ENTRY_POINT}
+    entry_points={'opm.stt': PLUGIN_ENTRY_POINT,
+                  'opm.stt.config': CONFIG_ENTRY_POINT}
 )
